f459efc8533b6742453050ca56ee11ee404ca405
hajizadehA <72498899+hajizadehA@users.noreply.github.com>
2022-01-21 17:49:16 +0330

    create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+require_once 'madeline.php';
+$MadelineProto = new \danog\MadelineProto\API("session.madeline");
+$MadelineProto->start();
+
+$channel = "-10044";
+$user = "152468";
+$2fa = "123654";
+
+try {
+    $info = (array)$MadelineProto->account->getPassword();
+    $hasher = new \danog\MadelineProto\MTProtoTools\PasswordCalculator($MadelineProto->API->logger);
+    $hasher->addInfo($info);
+    $Updates = $MadelineProto->channels->editCreator(['channel' => $channel, 'user_id' => $user, 'password' => $hasher->getCheckPassword($2fa)]);
+    print_r($Updates);
+   
+} catch (Exception $e) {
+    print_r($e);
+}
